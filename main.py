6e60b4e5369055878a96d6a56be40a60c550d085
phuochieu212 <lethanhphuochieu@gmail.com>
2022-03-10 10:42:04 +0700

    - Use relative path for all paths - Add another handling case for mouse dragging

--- main.py
+++ main.py
@@ -58,6 +58,8 @@ def do_ocr(
     # TODO: Handle different cases with start_point, end_point
     if start_point is None or end_point is None:
         return None
+    elif start_point == end_point:
+        return None
 
     crop = img.crop((*start_point, *end_point))
     text = ocr.ocr(np.asarray(crop), det=False)
@@ -77,8 +79,8 @@ if __name__ == "__main__":
         cls_model_dir=str(BASE_MODEL_PATH / "cls\\ch_ppocr_mobile_v2.0_cls_infer"),
         det_model_dir=str(BASE_MODEL_PATH / "det\\en\\en_ppocr_mobile_v2.0_det_infer"),
         rec_model_dir=str(BASE_MODEL_PATH / "rec\\en\\en_number_mobile_v2.0_rec_infer"),
-        e2e_char_dict_path=str(Path("utils/ic15_dict.txt")),
-        rec_char_dict_path=str(Path("utils/en_dict.txt")),
+        e2e_char_dict_path=str(Path("./utils/ic15_dict.txt")),
+        rec_char_dict_path=str(Path("./utils/en_dict.txt")),
         use_gpu=False,
         use_angle_cls=True,
         lang="en",
