610e18e637ab23b6171ae5a2e310459730dd8e07
phuochieu212 <lethanhphuochieu@gmail.com>
2022-03-15 23:14:07 +0700

    Decouple backend from main.py file

--- main.py
+++ main.py
@@ -1,69 +1,31 @@
 from pathlib import Path
-from typing import List, Tuple
 
-import numpy as np
 import PySimpleGUI as sg
-from fitz import Document, Matrix, Pixmap
-from paddleocr import PaddleOCR
-from PIL import Image
 
-from src.backend.pdf_processing import save_pdf, split_pdf
+from src.backend import Processor
 from src.frontend.ui import get_main_window, get_viz_window
 
 sg.theme("LightGrey1")
 sg.set_options(dpi_awareness=True)
 
 
-def get_page_as_img(pdf_file: Document):
-    pix: Pixmap = pdf_file.load_page(page_id=0).get_pixmap(matrix=Matrix(2.0, 2.0))
-    img = Image.frombytes("RGB", (pix.width, pix.height), pix.samples)
-    return img, pix.tobytes("ppm")
-
-
-def viz_next_doc(
-    graph: sg.Graph, pdf_files: List[Document], current_doc: int, img_id: int
-):
-    current_doc += 1
+def viz_next_doc(graph: sg.Graph, img_data, img_id: int):
     graph.delete_figure(img_id)
-    page_as_img, img_data = get_page_as_img(pdf_files[current_doc])
     img_id = graph.draw_image(data=img_data, location=(0, 0))
     graph.send_figure_to_back(img_id)
-    return page_as_img, current_doc, img_id
-
-
-def do_ocr(
-    ocr: PaddleOCR,
-    img: Image.Image,
-    start_point: Tuple[int, int],
-    end_point: Tuple[int, int],
-):
-    if start_point is None or end_point is None:
-        return None
-
-    x1, y1 = start_point
-    x2, y2 = end_point
-    if x1 == x2 or y1 == y2:
-        return None
-    if x1 > x2:
-        x1, x2 = x2, x1
-    if y1 > y2:
-        y1, y2 = y2, y1
-
-    crop = img.crop((x1, y1, x2, y2))
-    text = ocr.ocr(np.asarray(crop), det=False)
-    return text[0][0] if text else None
+    return img_id
 
 
 def do_info_update(window, current_doc, total_files, step=1, ocr_text=None):
     window["-INFO-"].update(value=f"Page {current_doc * step + 1}/{total_files * step}")
-    window["-OCR-STR-"].update(value=text if ocr_text is not None else "")
+    window["-OCR-STR-"].update(value=ocr_text if ocr_text is not None else "")
 
 
 if __name__ == "__main__":
     # Prepare the OCR model
     BASE_MODEL_PATH = Path(".paddleocr/2.4/ocr")
     BASE_PPOCR_UTILS_PATH = Path(".paddleocr/ppocr/utils")
-    ocr = PaddleOCR(
+    processor = Processor(
         cls_model_dir=str(BASE_MODEL_PATH / "cls/ch_ppocr_mobile_v2.0_cls_infer"),
         det_model_dir=str(BASE_MODEL_PATH / "det/en/en_ppocr_mobile_v2.0_det_infer"),
         rec_model_dir=str(BASE_MODEL_PATH / "rec/en/en_number_mobile_v2.0_rec_infer"),
@@ -84,14 +46,9 @@ if __name__ == "__main__":
     dragging = False
     start_point = end_point = prior_rect = None
 
-    destination_folder = None
-    pdf_filepaths = None
-    pdf_files = None
-    current_doc = 0
-    page_as_img = None
+    img_data = None
     img_id = None
     step = None
-    filename = None
 
     while True:
         window, event, values = sg.read_all_windows()
@@ -116,20 +73,21 @@ if __name__ == "__main__":
                     break
 
                 # Store the link to destination folder
-                destination_folder = Path(values["-OUT-DIR-"])
-                pdf_filepaths = values["-IN-PDFS-"]
+                processor.dst_folder = Path(values["-OUT-DIR-"])
                 step = 1 if values["-ONE-PAGE-"] else 3
-                pdf_files: List[Document] = split_pdf(pdf_filepaths, step=step)
+                processor.add_documents(values["-IN-PDFS-"], step)
 
                 mainWindow.hide()
 
                 # Init visualization window
-                page_as_img, img_data = get_page_as_img(pdf_files[current_doc])
+                img_data = next(processor)
                 vizWindow, graph, img_id = get_viz_window(
-                    page_as_img.height, page_as_img.width, img_data
+                    processor.img.height, processor.img.width, img_data
                 )
                 vizWindow["-OCR-STR-"].block_focus(block=True)
-                do_info_update(vizWindow, current_doc, len(pdf_files), step)
+                do_info_update(
+                    vizWindow, processor.current_doc, processor.total_docs, step
+                )
 
         if window == vizWindow:
             if event in (sg.WIN_CLOSED, "Exit", "Cancel"):
@@ -155,11 +113,17 @@ if __name__ == "__main__":
                 info = window["-INFO-"]
                 ocr_str = window["-OCR-STR-"]
 
-                text = do_ocr(ocr, page_as_img, start_point, end_point)
-                do_info_update(window, current_doc, len(pdf_files), step, ocr_text=text)
+                text = processor.ocr(start_point, end_point)
+                do_info_update(
+                    window,
+                    processor.current_doc,
+                    processor.total_docs,
+                    step,
+                    ocr_text=text,
+                )
 
                 dragging = False
-                start_point = end_point = None
+                start_point = end_point = None  # enable grabbing a new rect
             elif event in ("OK", "e"):  # "e,<Enter>" key will behave like an "OK" event
                 if (
                     event == "e"
@@ -167,20 +131,20 @@ if __name__ == "__main__":
                 ):  # Handle case where event "e" is in the input box
                     continue
 
-                pdf_name = values["-OCR-STR-"]
-                save_pdf(pdf_files[current_doc], destination_folder / f"{pdf_name}.pdf")
+                processor.save_document(values["-OCR-STR-"])
 
-                if current_doc + 1 < len(pdf_files):
-                    page_as_img, current_doc, img_id = viz_next_doc(
-                        graph, pdf_files, current_doc, img_id
-                    )
-                    start_point, end_point = graph.get_bounding_box(prior_rect)
-                    text = do_ocr(ocr, page_as_img, start_point, end_point)
+                try:
+                    img_data = next(processor)
+                    img_id = viz_next_doc(graph, img_data, img_id)
+                    text = processor.ocr(*graph.get_bounding_box(prior_rect))
                     do_info_update(
-                        window, current_doc, len(pdf_files), step, ocr_text=text
+                        window,
+                        processor.current_doc,
+                        processor.total_docs,
+                        step,
+                        ocr_text=text,
                     )
-                    start_point, end_point = None, None  # enable grabbing a new rect
-                else:
+                except StopIteration:
                     sg.popup(
                         "All files have been processed! Exit now...",
                         title="Notification",
---
+++ src/backend/ocr_processing.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from typing import List, Tuple
+
+import numpy as np
+from fitz import Document, Matrix, Pixmap
+from paddleocr import PaddleOCR
+from PIL import Image
+
+from .pdf_processing import save_pdf, split_pdf
+
+
+class Processor(PaddleOCR):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.dst_folder: Path = None
+        self.pdf_files: List[Document] = []
+        self.total_docs = 0
+        self.current_doc = -1
+        self.img = None  # Hold current image corresponding to current_doc
+
+    def add_documents(self, pdf_filepaths, step: int = 1):
+        self.pdf_files += split_pdf(pdf_filepaths, step=step)
+        self.total_docs += len(self.pdf_files)
+
+    def save_document(self, fname: str):
+        save_pdf(self.pdf_files[self.current_doc], self.dst_folder / f"{fname}.pdf")
+
+    def get_doc_as_img(self, doc_idx: int, page: int = 0, scale: float = 1.0):
+        pix: Pixmap = (
+            self.pdf_files[doc_idx]
+            .load_page(page_id=page)
+            .get_pixmap(matrix=Matrix(scale, scale))
+        )
+        img = Image.frombytes("RGB", (pix.width, pix.height), pix.samples)
+        return img, pix.tobytes("ppm")
+
+    def __next__(self):
+        if self.current_doc + 1 < self.total_docs:
+            self.current_doc += 1
+            self.img, img_data = self.get_doc_as_img(self.current_doc)
+            return img_data
+        else:
+            raise StopIteration
+
+    def ocr(
+        self,
+        start_point: Tuple[int, int],
+        end_point: Tuple[int, int],
+    ):
+        if start_point is None or end_point is None:
+            return None
+
+        x1, y1 = start_point
+        x2, y2 = end_point
+        if x1 == x2 or y1 == y2:
+            return None
+        if x1 > x2:
+            x1, x2 = x2, x1
+        if y1 > y2:
+            y1, y2 = y2, y1
+
+        crop = self.img.crop((x1, y1, x2, y2))
+        text = super().ocr(np.asarray(crop), det=False)
+        return text[0][0] if text else None
